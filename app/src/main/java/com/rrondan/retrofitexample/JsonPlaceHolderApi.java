51438a6adca14ef56bda4c839af8b5dbefa8de19
Richard Rondan Rojas <rondanrichard@gmail.com>
2019-06-30 07:24:57 -0500

    first commit

---
+++ app/src/main/java/com/rrondan/retrofitexample/JsonPlaceHolderApi.java
@@ -0,0 +1,13 @@
+package com.rrondan.retrofitexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+}
